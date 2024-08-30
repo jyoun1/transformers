a0ce4a0646ceb61e44d97a86a628763b12eac430
jyoun1 <127086375+jyoun1@users.noreply.github.com>
2024-08-30 16:33:00 +0900

    Update optimization.py

--- src/transformers/optimization.py
+++ src/transformers/optimization.py
@@ -138,7 +138,7 @@ def _get_cosine_schedule_with_warmup_lr_lambda(
     if current_step < num_warmup_steps:
         return float(current_step) / float(max(1, num_warmup_steps))
     progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
-    return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
+    return max(0.0000001, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
 
 
 def get_cosine_schedule_with_warmup(
